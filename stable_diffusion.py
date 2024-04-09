266fe24f77c6cdff1683f531862e9c8d1dc43951
Esmail Atta <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-04-09 20:20:26 +0300

    Update stable_diffusion.py

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -10,7 +10,7 @@ class GaussingDistribution:
     def sample(self):
         return self.mean + self.std * torch.rand_like(self.std)
 
-class DDPMSampler:
+class DenoisingDiffusionProbabilisticModelSampler:
     def __init__(self, generator: torch.Generator, number_training_steps = 1000, beta_start: float = 0.00085, beta_end: float = 0.0120) -> None:
         self.betas = torch.linspace(beta_start ** 0.5, beta_end ** 0.5, number_training_steps, dtype=torch.float32) ** 2 
         self.alphas = 1.0 - self.betas
